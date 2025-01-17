0cb2f11c2d5d5932d808cc8799f72a1056db9475
Lajoie, Samuel <lajoie.samuel@courrier.uqam.ca>
2025-01-17 13:29:46 -0500

    Added some game assets and a default test map

---
+++ assets/pixel_isometric_tileset.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.1" name="pixel_isometric_tileset" tilewidth="32" tileheight="32" tilecount="121" columns="11">
+ <grid orientation="isometric" width="32" height="32"/>
+ <image source="spritesheet.png" width="352" height="352"/>
+</tileset>
